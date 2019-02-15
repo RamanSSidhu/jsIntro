f1b2869b052e50335c2c795d7e7a59947747a5f0
RamanSidhu <ramanpreet.sidhu@ucalgary.ca>
2019-02-15 15:27:40 -0700

    Initial js code

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
 $(document).ready(() => {
-    const prevExpression = $("#previous-expression");
-    const currExpression = $("#current-expression");
-    
+    // Set Previous and Current Expression to paragraph and form
+    const prevExpression = $(`#previous-expression`);
+    const currExpression = $(`#current-expression`);
+
+    // State Variables
+    let unclosedBrackets = 0;
+    let clearedState = true;
+    let decimalAllowed = true;
+    let isPrevMathFunction = false;
+
+    // Number button handler
+    $('.btn-number').click(event => {
+        let btnValue = event.target.textContent;
+
+        if (!clearedState) {
+            currExpression.text(currExpression.text() + btnValue);
+        }
+        else
+        {
+            currExpression.text(btnValue);
+        }
+        
+        if (currExpression.text != "0")
+        {
+            clearedState = false;
+        }
+    })
+
+    // Function button handler
+    $('.btn-function').click(event => {
+        let btnValue = event.target.textContent;
+
+        if (isPrevMathFunction)
+        {
+            currExpression.text.slice(0, -1) + btnValue;
+        }
+        else
+        {
+            currExpression.text += btnValue;
+        }
+    })
+
+
+    // Equals button handler
+    $('#equals-button').click(() => {
+        prevExpression.text = currExpression.text;
+        currExpression.text = eval(currExpression.text);
+    })
+
 })
